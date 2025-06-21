691d5eefd79cee7e166f9260a3a7689470514cf2
Vinny65868777q <141133050+Vinny65868777q@users.noreply.github.com>
2025-06-21 16:56:50 +0530

    Added User Profile API and LeaderBoard API

--- backend/controllers/submissionController.js
+++ backend/controllers/submissionController.js
@@ -28,6 +28,47 @@ const createSubmission = async (req, res) => {
     }
 
 };
+//Returns all submissions by the logged-in user — basically their submission history or list.
+const getUserSubmission = async (req, res) => {
+    try {
+
+        const userId = req.user.id;
+        const submission = await Submission.find({ userId })//query the MongoDB database.
+            .populate('problemId', 'title difficulty')//replaces the problemId field (which is usually just an ObjectId) with the the title and difficulty fields.//Mongoose will also include the problem id
+            .sort({ submittedAt: -1 });//the most recent submissions appear first.
+
+        res.status(200).send(submission);
+
+    } catch (error) {
+        console.error("Error fetching user submissions:", error);
+        res.status(500).send("Server error while fetching submissions");
 
 
-module.exports = { createSubmission };
+    }
+
+};
+
+//Returns one specific submission by its unique ID — used when you want to see details of a particular submission (like code, verdict, timestamps).
+
+const getSubmissinByID = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const submission = await Submission.findById(id)
+            .populate('problemId', 'title difficulty')
+            .populate('userId', 'firstname lastname');
+
+        if (!submission) {
+            return res.status(404).send('Submission not Found');
+        }
+        res.status(200).send(submission);
+
+    } catch {
+        console.error("Error fetching submission:", error);
+        res.status(500).send( "Server error while fetching submission" );
+
+    }
+
+
+};
+
+module.exports = { createSubmission, getUserSubmission,getSubmissinByID };
